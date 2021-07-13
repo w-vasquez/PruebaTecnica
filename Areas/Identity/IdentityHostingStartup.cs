14ada526462bce489fc27e6c99606397ce164b8b
w-vasquez <2740882018@mail.utec.edu.sv>
2021-07-12 22:29:35 -0600

    Primer snapshot Identity, api google y ipinfo desde javascript

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PruebaTecnica.Areas.Identity.Data;
+using PruebaTecnica.Data;
+
+[assembly: HostingStartup(typeof(PruebaTecnica.Areas.Identity.IdentityHostingStartup))]
+namespace PruebaTecnica.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDBContextConnection")));
+
+                services.AddDefaultIdentity<PruebaTecnicaUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AuthDBContext>();
+            });
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PruebaTecnica.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using IpInfo;
+
+namespace PruebaTecnica.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect("~/");
+        }
+
+        Array str_array;
+        public async Task<Array> Location()
+        {
+            using var client = new HttpClient();
+            var api = new IpInfoApi("5b1c9ba962808b", client); // Some methods work without a token, for this case there is a constructor without a token.
+
+            var response = await api.GetCurrentInformationAsync();
+            str_array = response.Loc.Split(',');
+            return str_array;
+        }
+
+
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+    
+}
